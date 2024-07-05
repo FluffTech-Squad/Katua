f8da6a33b8448e9de10e6a9e2a67ec97e20c352d
Nekomancer <114854571+nekomancer0@users.noreply.github.com>
2024-07-05 14:40:01 +0200

    test

--- index.js
+++ index.js
@@ -10,6 +10,12 @@ if (child_process.execSync("git status --porcelain").toString() !== "") {
   process.exit(1);
 }
 
+// Set origin repository
+
+child_process.execSync("git init");
+
+child_process.execSync("git remote add origin " + process.env.REPOSITORY);
+
 child_process.execSync("git pull");
 
 child_process.execSync("node --require ./appsignal.cjs ./bot.js");
